880410e4a7c9d3a95e76dc0293cb6fbb70b9f5b7
ste46751 <stefanquarti060@gmail.com>
2023-11-20 09:58:47 +0100

    Aggiungere i file di progetto.

---
+++ 2RappresentareUnaPersonaFisica/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2RappresentareUnaPersonaFisica
+{
+    internal class Program
+    {
+        public class Dati
+        {
+            protected int codice;
+            protected string congome;
+            protected string nome;
+            protected bool registrato;
+
+            public Dati(int codice, string congome, string nome, bool registrato)
+            {
+                this.codice = codice;
+                this.congome = congome;
+                this.nome = nome;
+                this.registrato = registrato;
+            }
+
+        }
+        public void ControlloReg()
+        {
+            Console.WriteLine("Vuoi registrare? (sì, no)");
+            string c=Console.ReadLine();
+
+            if(c== "si" || c=="sì")
+            {
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
