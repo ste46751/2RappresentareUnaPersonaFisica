5ee42c513e654fb86b3a43de9da90d76fbe5ced9
ste46751 <stefanquarti060@gmail.com>
2023-11-21 10:31:45 +0100

    fine

--- 2RappresentareUnaPersonaFisica/Program.cs
+++ 2RappresentareUnaPersonaFisica/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static _2RappresentareUnaPersonaFisica.Program;
 
 namespace _2RappresentareUnaPersonaFisica
 {
@@ -12,32 +14,51 @@ namespace _2RappresentareUnaPersonaFisica
         public class Dati
         {
             protected int codice;
-            protected string congome;
+            protected string cognome;
             protected string nome;
             protected bool registrato;
 
-            public Dati(int codice, string congome, string nome, bool registrato)
+            public Dati()
             {
-                this.codice = codice;
-                this.congome = congome;
-                this.nome = nome;
-                this.registrato = registrato;
+                codice = 0303030;
+                cognome = "Morandi";
+                nome = "Gianni";
+                registrato = false;
             }
 
-        }
-        public void ControlloReg()
-        {
-            Console.WriteLine("Vuoi registrare? (sì, no)");
-            string c=Console.ReadLine();
+            public void ControlloReg()
+            {
+                Console.WriteLine("Vuoi registrare? (sì, no)");
+                string c = Console.ReadLine();
 
-            if(c== "si" || c=="sì")
+                if (c == "si" || c == "sì")
+                {
+                    registrato = true;
+                }
+                else
+                {
+                    registrato = false;
+                }
+            }
+
+            public void stamp()
             {
                 
+                Console.WriteLine(codice);
+                Console.WriteLine(cognome);
+                Console.WriteLine(nome);
+
+                Console.WriteLine(registrato? "si":"no");
             }
         }
+
         static void Main(string[] args)
         {
 
+            Dati d = new Dati();
+            d.stamp();
+            d.ControlloReg();
+            d.stamp();
         }
     }
 }
